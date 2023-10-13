8e5990dd0a67b60c1377d37ea0cc42979db28776
sik2 <devpang20@gmail.com>
2023-10-13 21:05:17 +0900

    EditorPage, Editor 2차 구현

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,15 @@ export default function App ({ $target }) {
 
     const editorPage = new EditorPage({ 
         $target: $rendingContainer, 
-        initialState: {}
+        initialState: {
+            postId: 'new',
+            post: {
+                title: '',
+                content: ''
+            }
+        }
     })
+
+    // editorPage.setState();
 }
 
